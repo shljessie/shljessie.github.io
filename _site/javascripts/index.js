6886bdb7b22271a7f78bf9c50756abf14aa7d17a
shljessie <sl994@cornell.edu>
2022-12-08 13:33:51 -0500

    restore

--- _site/javascripts/index.js
+++ _site/javascripts/index.js
@@ -1,32 +1,32 @@
 
 
 
-// new p5(function(p) {
-//   var t = 0;
-//   var d = 250;
+new p5(function(p) {
+  var t = 0;
+  var d = 250;
 
-//   p.setup = function() {
-//     var canvas = p.createCanvas(p.windowWidth, p.windowHeight);
-//     p.stroke(p.color('lightblue'), 120);
-//     p.noFill();
-//     p.frameRate(20);
-//   }
+  p.setup = function() {
+    var canvas = p.createCanvas(p.windowWidth, p.windowHeight);
+    p.stroke(p.color('lightblue'), 120);
+    p.noFill();
+    p.frameRate(20);
+  }
 
-//   p.draw = function() {
-//     p.background(255, 15);
-//     p.beginShape();
-//     for (var i = -1; i <= d; i++) {
-//       var x = p.map(i, 0, d, 0, p.width);
-//       var y = p.map(p.noise(i * 0.01, t * 0.005), 0, 1, 0, p.height) + p.map(i, 0, d, -p.height/4, p.height/2);
-//       p.curveVertex(x, y);
-//     }
-//     p.endShape(p.OPEN);
+  p.draw = function() {
+    p.background(255, 15);
+    p.beginShape();
+    for (var i = -1; i <= d; i++) {
+      var x = p.map(i, 0, d, 0, p.width);
+      var y = p.map(p.noise(i * 0.01, t * 0.005), 0, 1, 0, p.height) + p.map(i, 0, d, -p.height/4, p.height/2);
+      p.curveVertex(x, y);
+    }
+    p.endShape(p.OPEN);
 
-//     t += 1;
-//   }
+    t += 1;
+  }
 
-//   p.windowResized = function() {
-//     p.resizeCanvas(p.windowWidth, p.windowHeight);
-//   }
-// }, document.querySelector('body'));
+  p.windowResized = function() {
+    p.resizeCanvas(p.windowWidth, p.windowHeight);
+  }
+}, document.querySelector('body'));
 
